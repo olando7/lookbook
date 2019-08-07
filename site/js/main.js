7b589e021a482b7a130372d90487c6bf32733592
olando7 <olandio7@gamil.com>
2019-08-07 10:41:47 +0900

    detail

--- site/js/main.js
+++ site/js/main.js
@@ -1,44 +1,46 @@
 $(function(){
-   
-   $searchToggle = $('.search_toggle');
-   $searchForm = $('.main-header form');
+    $searchToggle = $('.search_toggle');
+    $searchForm = $('.main-header form');
 
-   $searchForm.find('button').prop('disabled', true);
+    $searchForm.find('button').prop("disabled", true);
 
-   $searchToggle.click(function(){
-      $searchForm.toggleClass('active');
-      if($searchForm.hasClass('active')){
-         $searchForm.find('button').prop('disabled', false);
-      }else{
-         $searchForm.find('button').prop('disabled', true);
-      }
-   });
+    $searchToggle.click(function(){
+        $searchForm.toggleClass('active');
+        if($searchForm.hasClass('active')){
+            $searchForm.find('button').prop("disabled", false);
+        }else{
+            $searchForm.find('button').prop("disabled", true);
+        }        
+    });
 
-   $asideToggleBtn = $('.aside_toggle');
-   $asideBg = $('.aside_bg');
+    //aside toggle
+    $body = $('body');
+    $asideToggleBtn = $('.aside_toggle');
+    $sideCloseBtn = $('.aside_bg');
 
-   $asideToggleBtn.click(function(){
-      $('body').addClass('aside_open');
-   });
-   $asideBg.click(function(){
-      $('body').removeClass('aside_open');
-   });
-   
-   //category change view
+    $asideToggleBtn.click(function(){
+        $body.addClass('aside-open');
+    });
+    $sideCloseBtn.click(function(){
+        $body.removeClass('aside-open');
+    });
 
-   var $listToggle = $('.list_toggle'),
-       $category = $('.category');
+    //Categroy change view
+    var $listToggle = $('.list_toggle'),
+        $category = $('.category');
 
-   $listToggle.click(function(){
-      $(this).add($category).toggleClass('active');
-   });
+    $listToggle.click(function(){
+        $(this).add($category).toggleClass('active');
+        //$('.list_toggle, .category').toggleClass('active');
+    })
 
-   //이전페이지로 이동
-   var $backwardsBtn = $('.backwards_toggle');
+    //이전 페이지로 이동하기
+    var $backwardsBtn = $('.backwards_toggle');
 
-   $backwardsBtn.click(function(){
-      history.go(-1);
-      //history.go(1); history.back();
-   });
+    $backwardsBtn.click(function(){
+        history.go(-1);
+        //history.go(1);  history.back(); history.forward();
+    });
+
 
 });
